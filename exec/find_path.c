737431124c6b6168e548cfc6b90787dd8c4b4c9d
Ahmed El Mountassir <49590486+mountassir-007@users.noreply.github.com>
2022-02-23 17:08:13 +0100

    executing commands

---
+++ exec/find_path.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_bin_path.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahel-mou <ahel-mou@1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/23 12:54:40 by ahel-mou          #+#    #+#             */
+/*   Updated: 2022/02/23 16:59:50 by ahel-mou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+#include "../libft/libft.h"
+
+/* 
+	checks if the user input is a command that exist in one the paths int the PATH var
+*/
+char	*find_path(char *command)
+{
+	char **paths;
+	char *cmd;
+	char *path;
+	
+	paths = ft_split(getenv("PATH"), ':'); 
+	// ^ : splits the PATH var into multiple paths so we can check if that command exist in one of them
+	
+    cmd = ft_strjoin("/", command); // joins the user input with a '/' so we can join is here with one of the paths and check if that cmd exist in the path[i]
+    while (*paths) //																	  |
+    {			//						strjoin  		paths[0] = /bin  cmd = /ls	      |
+        path = ft_strjoin(*paths, cmd);// 	 <============================================| path = /bin/ls
+        if (!path)
+            return (NULL);
+        if (!access(path, F_OK)) // checks if the command exist in the current path if yes it breaks the loop and returns the path
+            break ;
+        free(path);
+        paths++;
+    }
+    free(cmd);
+    return (path);
+}
