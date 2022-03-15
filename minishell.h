04e69f7f7c6b2db530a12b96bbaf99737de70385
Ahmed El Mountassir <49590486+mountassir-007@users.noreply.github.com>
2022-03-15 17:09:21 +0100

    header file

--- minishell.h
+++ minishell.h
@@ -5,55 +5,120 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahel-mou <ahel-mou@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/18 21:33:40 by ahel-mou          #+#    #+#             */
-/*   Updated: 2022/02/27 16:35:50 by ahel-mou         ###   ########.fr       */
+/*   Created: 2022/03/02 16:33:06 by ahel-mou          #+#    #+#             */
+/*   Updated: 2022/03/15 15:23:34 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h> // sigaction | signal ...
-#include <string.h>
-#include <dirent.h> //open dir | clode dir
-#include <readline/readline.h> // rl
-#include <readline/history.h> // rl
-#include <fcntl.h>
+# include "struct.h"
+# include "./libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/ioctl.h>
+# include <sys/stat.h>
+# include <termios.h>
+# include <errno.h>
+# include <string.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
-typedef struct minishell
-{
-	char		*rl_return; //  user input
-	char		**env; 		// envirement variavle
-	char		**export;
-}				t_vars;
+# define READ 0
+# define WRITE 1
+# define MAX 1000
 
-//----------------------------------------------------->Minishell Utils
-void		exec_cmd(t_vars *unit, char **cmd);
-char		*find_path(char *command);
-int			cmp_with_homemade(char **cmd, t_vars *unit);
-void		ft_free_td(char **td_arr);
-int			pipex(int num_of_cmds, char **commands, char **env);
-//-----------------------------------------------------> parasing utils
-void		handle_rl_input(t_vars   *unit); //--> 
-char		*rm_space(char *cmd);
 
-//-----------------------------------------------------> commands
-int		ft_unset(t_vars *unit);
-int		ft_export(t_vars *unit);
-int		ft_cd(char **path);			// done
-int		ft_exit(t_vars	*unit); 	// done 
-int		ft_pwd(t_vars *unit);		// done
-int		ft_env(t_vars *unit); 		// done
-void	env_var(char **cmd);		// done
+/*
+ * PARSING
+ */
+int		split_commands(t_shell *shell, char *line); 
+// split usser input with pipes into multiple commands
+void	check_var_or_retval(t_shell *s, char **arg, int i); 
+// checks if there is a $var and replace it with its vaue or if there is a $? last_retval
+char	**split_cmd_args(t_shell *sh, char *command, char c);
+// split a cmd with its args in a 2d_arr
+char	**parse_cmd(t_shell *s, int i);
+// split 1 cmd into a 2d_arr with its arguments using space ex: ls -al    2d_arr = [0] = ls | [1] = -al
+void	check_dollar(t_shell *s, char **arg, int i);
+void	check_quotes(t_shell *s, char **arg, int i);
 
-//---------//->echo
-int		ft_echo(char **cmd); // loading
-void    quotes_f_checker(int *quotes, int *checker, char cara); //echo utils
-void    quotes_t_checker(int *quotes, int *checker, char cara); // echo utils
-void    white_spaces(char cara); //echo utils
-//---------//
+
+
+//Home made commands
+void	exit_cmd(t_shell *shell);
+void	cd_cmd(t_shell *shell);
+void	tilde_handler(t_shell *shell); // handle the 'cd -'
+void    display_export(t_shell *shell);
+void	unset_cmd(t_shell *shell);
+void	export_cmd(t_shell *shell);
+char	**split_export_cmd(char const *str); // we use this function to split the command export into multipl words ||"cmd" "name" "args"|| ..
+
+/*
+ * EXECUTION
+ */
+void	start_shell(t_shell *s); //pre_executing part
+void	execution(t_shell *s); // execution
+int		only_hrdoc_with_nocmd(t_shell *s); // checks id there is only a heredoc without a cmd after it
+int		not_executable_cmd(t_shell *s, char *cmd); // checks if the command is executable
+
+int		not_pipeable_cmd(t_shell *s); // checks if the commands is pipe able means that it has an output
+void	checkfile_redir(t_shell *s);
+void	get_fds(t_shell *s, int i, int *pipe_fd);
+void	get_heredoc(t_shell *s, char *word);
+void	fork_failed(t_shell *s);
+void	close_fds(t_shell *s);
+
+/*
+ * ENVIRONMENT
+ */
+
+int		change_var_value(t_shell *shell, char *var, char *value); // change *var value with *value
+char	*get_envar_value(t_shell *shell, char *str); // returns the valus of the env variable
+void	enver(t_shell *shell);
+void	expoort(t_shell *shell, char *str);
+char	*get_val(char *str);
+int		alpharank(t_shell *shell, char *str);
+void	sort_env(t_shell *shell);
+int		is_sorted(char *s1, char *s2);
+// void	dollar(t_shell *shell);
+void	shell_levels(t_shell *shell);
+
+/*
+ * UTILS
+ */
+void	init_structure(t_shell *s, char **env);
+void	reinit_structure(t_shell *s);
+int		go_next_quote(t_shell *sh, char *cmd, int i, int quotes);
+char	**split_cmd_ags(t_shell *sh, char *command, char c);
+int	dbquote_check(char *cmd, int i, int quote);
+int		is_quote(char *s, int i, int quotes);
+
+void	free_arr(char **array);
+void	free_redir(t_shell *s);
+int		ft_exit(t_shell *shell, char *err);
+int		starts_with(char *s1, char *s2);
+int		valid_dbquote(char *s, int j, int Q);
+int		not_doublequote(char *s, int j);
+void	wait_pid(void);
+void	reset_string(char **s, int i);
+void	reset_shell(t_shell *s);
+void	free_struct(t_shell *s);
+void	ft_free(char *s);
+void	str_replace(char **dst, char *src);
+void	swap_file(t_shell *s, char **file, char **arg, int i);
+
+
+//ERRORS
+void	error_cmd_not_found(t_shell *s, char *cmd); //cmd not fount error
+int		bash_error_unexpected_token(t_shell *s, int err);
+void	bash_error_w_filename(t_shell *s, char *file);
+void	quotes_error(t_shell *s);
 
 #endif
